d9095338b5bb0841258dcfe620596336c523bf7a
Sam Fonseca <samfonseca@utexas.edu>
2016-05-03 02:49:55 -0500

    allow user to specify listening port and db file with DINGO_PORT and DINGO_DB_FILE env vars

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/dinever/dingo/app"
 )
 
 func main() {
-	portPtr := flag.String("port", "8000", "The port number for Dingo to listen to.")
-	dbFilePathPtr := flag.String("database", "dingo.db", "The database file path for Djingo to use.")
+	listenPort := os.Getenv("DINGO_PORT")
+	if listenPort == "" {
+		listenPort = "8000"
+	}
+	portPtr := flag.String("port", listenPort, "The port number for Dingo to listen to.")
+
+	dbFile := os.Getenv("DINGO_DB_FILE")
+	if dbFile == "" {
+		dbFile = "dingo.db"
+	}
+	dbFilePathPtr := flag.String("database", dbFile, "The database file path for Djingo to use.")
+
 	flag.Parse()
 
 	Dingo.Init(*dbFilePathPtr)
