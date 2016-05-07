adefc12d0c2cc7e8909aa5d26ecca598aede5da1
dinever <dingpeixuan911@gmail.com>
2016-05-07 02:40:56 -0400

    [fix] Fix del-nav button not functioning properly.

--- view/admin/assets/src/js/settings.js
+++ view/admin/assets/src/js/settings.js
@@ -11,34 +11,22 @@ $(function () {
         e.preventDefault();
         $("#custom-settings").append($($(this).attr("rel")).html());
         componentHandler.upgradeDom();
-        $(".del-custom").on("click", function(e) {
-            e.preventDefault();
-            var item = $(this).parent().parent()
-            alertify.confirm("Delete this item?", function() {
-                item.remove();
-            });
-        });
     });
     $("#add-nav").on("click", function(e) {
         e.preventDefault();
         $("#navigators").append($($(this).attr("rel")).html());
         componentHandler.upgradeDom();
-        $(".del-nav").on("click", function(e) {
-            e.preventDefault();
-            var item = $(this).parent().parent()
-            alertify.confirm("Delete this item?", function() {
-                item.remove();
-            });
-        });
+
     });
-    $(".del-nav").on("click", function(e) {
+    $('.setting-form').on("click", ".del-nav", function(e) {
         e.preventDefault();
+        console.log($(this).parent().parent());
         var item = $(this).parent().parent()
         alertify.confirm("Delete this item?", function() {
             item.remove();
         });
     });
-    $(".del-custom").on("click", function(e) {
+    $('.setting-form').on("click", ".del-custom", function(e) {
         e.preventDefault();
         var item = $(this).parent().parent()
         alertify.confirm("Delete this item?", function() {
