f9ca7e78d5674ebb8852fb99c1a147ad96e6a40d
dinever <dingpeixuan911@gmail.com>
2016-05-09 14:01:51 -0400

    [fix] Extract profile page JS

---
+++ view/admin/assets/src/js/profile.js
@@ -0,0 +1,22 @@
+$(function(){
+    new FormValidator("profile-form",[
+        {"name":"slug","rules":"alpha_numeric|min_length[1]|max_length[20]"},
+        {"name":"email","rules":"valid_email"},
+        {"name":"url","rules":"valid_url"}
+    ],function(errors,e) {
+        e.preventDefault();
+        $('.invalid').hide();
+        if(errors.length){
+            $("#"+errors[0].id+"-invalid").removeClass("hide").show();
+            return;
+        }
+        $('#profile').ajaxSubmit(function(json){
+            if(json.status === "error"){
+                alert(json.msg);
+            }else{
+                alertify.success("Profile saved")
+            }
+            return false;
+        });
+    })
+});
